7e69db25a922c42f4b011e67fe96afebbbe7ea5a
Varkeychan Jacob <varkey98@gmail.com>
2023-12-11 02:40:46 +0000

    updating instrumentation

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -13,149 +13,70 @@ const int DefaultBufferThreshold = 1024 * 30;
 static string getRequestBody(Stream body)
 {
     string bodyText="";
-    body.Position = 0;
+    // body.Position = 0;
     var bodyStream = new StreamReader(body);
-    if (body.CanSeek) body.Seek(0, SeekOrigin.Begin);
+    // if (body.CanSeek) body.Seek(0, SeekOrigin.Begin);
     if (body.CanRead) bodyText = bodyStream.ReadToEndAsync().Result;
     return bodyText;
 }
 
-string getResponseBody(Stream body)
-{
-    var buffer = new MemoryStream();
-    body.CopyToAsync(buffer);
-    buffer.Position = 0L;
+var builder = WebApplication.CreateBuilder(args);
 
-    Console.WriteLine("Reading Body");
-    string bodyText="";
-    var bodyStream = new StreamReader(buffer);
-    if (buffer.CanSeek) body.Seek(0, SeekOrigin.Begin);
-    if (buffer.CanRead) bodyText = bodyStream.ReadToEndAsync().Result;
-
-    return bodyText;
-}
-
-void Enrich(Activity activity, HttpRequest request)
-{
-
-    ReadResponseBody(request.HttpContext.Response, body => {
-        activity.AddTag("v3", body);
-    });
-}
-
-void ReadResponseBody(HttpResponse res, Action<string> onReadBody)
-{
-    var context = res.HttpContext;
-    var priorFeature = context.Features.Get<IHttpResponseBodyFeature>();
-
-    if (priorFeature == null) return;
-    RecyclableMemoryStreamManager _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
-
-    var bufferedResponseStream = _recyclableMemoryStreamManager.GetStream();
-    var streamResponseBodyFeature = new StreamResponseBodyFeature(bufferedResponseStream, priorFeature);
-
-    context.Features.Set<IHttpResponseBodyFeature>(streamResponseBodyFeature);
-
-    context.Response.OnStarting(async () => {
-        try {
-            if (bufferedResponseStream.TryGetBuffer(out var data) && data.Array != null) {
-                var responseBody = Encoding.UTF8.GetString(data.Array, 0, data.Count);
-                onReadBody(responseBody);
-            }
-        } finally {
-            var originalStream = streamResponseBodyFeature.PriorFeature?.Stream;
-            if (originalStream is not null) {
-                bufferedResponseStream.Position = 0;
-                await bufferedResponseStream.CopyToAsync(originalStream).ConfigureAwait(false);
-            }
-
-            await bufferedResponseStream.DisposeAsync();
-        }
-    });
-}
-
-
-
-void EnableRewind(HttpResponse response, int bufferThreshold = DefaultBufferThreshold, long? bufferLimit = null)
-{
-    ArgumentNullException.ThrowIfNull(response);
-
-    var body = response.Body;
-    if (!body.CanSeek)
+// dependency injection
+builder.Services.AddOpenTelemetry()
+    .WithTracing(b =>
     {
-        var fileStream = new FileBufferingReadStream(body, bufferThreshold, bufferLimit, "./");
-        response.Body = fileStream;
-        // check alternatives
-        // request.HttpContext.Response.RegisterForDispose(fileStream);
-    }
-}
+        b
+            .AddAspNetCoreInstrumentation()
+            .AddOtlpExporter(opts => {
+                opts.Endpoint = new Uri("http://34.168.86.196:4317");
+            })
+            .AddConsoleExporter();
+        
+    });
+var app = builder.Build();
+
+
+
 
 
 // TODO body is not coming in case of AspNetCore Instrumentation
 // TODO request details are not coming in case of HttpClient Instrumentation
 // TODO learn async, await, task, ?
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOpenTelemetry()
-    .WithTracing(b =>
-    {
-        b
-            .AddAspNetCoreInstrumentation(options =>
-            {
-                options.EnrichWithHttpRequest = Enrich;
-                options.EnrichWithHttpResponse = (activity, response) =>
-                {
-                    // Task responseCapturer = CaptureResponse(response, activity);
-                    // Func<Task> myFun = async () => await responseCapturer;
-                    // response.OnStarting(myFun); 
-                    
-                    activity.SetTag("v2", getRequestBody(response.Body));
-                };
-            })
-            .AddConsoleExporter();
-        
-    });
-
-var app = builder.Build();
 
 var httpClient = new HttpClient();
 
-app.MapGet("/hello", async (context) =>
+
+app.MapPost("/hello", async delegate(HttpContext context)
 {
+    Console.WriteLine("I'm here even after getting blocked!!");
     HttpRequest request = context.Request;
     
-    // HttpRequestRewindExtensions.EnableBuffering(request);
-    
-    // Console.WriteLine(getRequestBody(request.Body));
-    // Console.WriteLine("Printing body again!!!");
-    // Console.WriteLine(getRequestBody(request.Body));
+    StreamReader reader = new StreamReader(context.Request.Body, Encoding.UTF8);
+    string jsonstring = getRequestBody(context.Request.Body);
 
-    var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://catfact.ninja/fact");
-    requestMessage.Headers.Add("jacob", new []{"test"});
-    var response = await httpClient.SendAsync(requestMessage);
-    // var html = await httpClient.GetStringAsync("https://example.com/");
+    // var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://catfact.ninja/fact");
+    // requestMessage.Headers.Add("jacob", new []{"test"});
+    // var response = await httpClient.SendAsync(requestMessage);
+    // // var html = await httpClient.GetStringAsync("https://example.com/");
 
-    string body = response.Content.ReadAsStringAsync().Result;
+    // string body = response.Content.ReadAsStringAsync().Result;
+
+    string body = jsonstring;
+    context.Response.ContentType = "text/plain";
     
-    context.Response.OnStarting(async () =>
-    {
-        EnableRewind(context.Response);
-    });
     if (string.IsNullOrWhiteSpace(body))
     {
         await context.Response.Body.WriteAsync(Encoding.ASCII.GetBytes("Hello World!"));
+        context.Response.ContentLength = 11;
     }
     else
     {
         await context.Response.Body.WriteAsync(Encoding.ASCII.GetBytes(body));
+        context.Response.ContentLength = body.Length;
+
     }
-    
-    context.Response.OnCompleted(async () =>
-    {
-        Console.WriteLine("Completed");
-        Console.WriteLine(getResponseBody(context.Response.Body));
-    });
 });
 
 // app.Use(async (context, next) =>
@@ -165,3 +86,4 @@ app.MapGet("/hello", async (context) =>
 // });
 
 app.Run();
+
--- HttpServer/scratch.sh
+++ HttpServer/scratch.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env bash
+#!\usr\bin\env bash
 
 #clean
 #dotnet nuget locals all --clear
 #dotnet restore
 
 #add
-dotnet add package OpenTelemetry.Api -s /Users/varkeychanjacob/Projects/opentelemetry-dotnet/src/OpenTelemetry.Api/bin/Debug --prerelease
-dotnet add package OpenTelemetry.Api.ProviderBuilderExtensions -s /Users/varkeychanjacob/Projects/opentelemetry-dotnet/src/OpenTelemetry.Api.ProviderBuilderExtensions/bin/Debug --prerelease
-dotnet add package OpenTelemetry -s /Users/varkeychanjacob/Projects/opentelemetry-dotnet/src/OpenTelemetry/bin/Debug --prerelease
-dotnet add package OpenTelemetry.Instrumentation.Http -s /Users/varkeychanjacob/Projects/opentelemetry-dotnet/src/OpenTelemetry.Instrumentation.Http/bin/Debug --prerelease
-dotnet add package OpenTelemetry.Exporter.Console -s /Users/varkeychanjacob/Projects/opentelemetry-dotnet/src/OpenTelemetry.Exporter.Console/bin/Debug --prerelease
-dotnet add package OpenTelemetry.Extensions.Hosting -s /Users/varkeychanjacob/Projects/opentelemetry-dotnet/src/OpenTelemetry.Extensions.Hosting/bin/Debug --prerelease              
-dotnet add package OpenTelemetry.Instrumentation.AspNetCore -s /Users/varkeychanjacob/Projects/opentelemetry-dotnet/src/OpenTelemetry.Instrumentation.AspNetCore/bin/Debug --prerelease
+dotnet add package Microsoft.IO.RecyclableMemoryStream
+dotnet add package OpenTelemetry.Api -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry.Api\bin\Debug --prerelease
+dotnet add package OpenTelemetry.Api.ProviderBuilderExtensions -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry.Api.ProviderBuilderExtensions\bin\Debug --prerelease
+dotnet add package OpenTelemetry -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry\bin\Debug --prerelease
+dotnet add package OpenTelemetry.Instrumentation.Http -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry.Instrumentation.Http\bin\Debug --prerelease
+dotnet add package OpenTelemetry.Exporter.Console -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry.Exporter.Console\bin\Debug --prerelease
+dotnet add package OpenTelemetry.Extensions.Hosting -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry.Extensions.Hosting\bin\Debug --prerelease              
+dotnet add package OpenTelemetry.Instrumentation.AspNetCore -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry.Instrumentation.AspNetCore\bin\Debug --prerelease
+dotnet add package OpenTelemetry.Exporter.OpenTelemetryProtocol -s C:\Users\varkeychan_jacob\dotnetagent\opentelemetry-dotnet\src\OpenTelemetry.Exporter.OpenTelemetryProtocol\bin\Debug --prerelease
