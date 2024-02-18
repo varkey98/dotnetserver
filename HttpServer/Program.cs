e548c1171cee7b4f96c242be000e7703ef04ebf8
Varkeychan Jacob <varkeychanjacob@Varkeychans-MacBook-Pro-2631.local>
2024-02-18 10:52:48 +0530

    adding ta back

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -1,49 +1,113 @@
 using System.Text;
 using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddLogging();
+var ta = Traceable.TraceableAgentBuilder.CreateBuilder().AddAspNetCoreInstrumentation(builder.Services).Build();
+builder.Services.AddSingleton(ta);
 var app = builder.Build();
 
-
-app.MapPost("/hello", async delegate(HttpContext context)
+app.MapPost("/hello", async delegate (HttpContext context)
 {
-    Console.WriteLine("I'm here even after getting blocked!!");
+    long start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
     HttpRequest request = context.Request;
-    
-    Request req = new Request();
+
+    // parse request 
+    Request req = new();
     using (var sr = new StreamReader(context.Request.Body))
     {
         string requestJson = sr.ReadToEndAsync().Result;
         req = JsonConvert.DeserializeAnonymousType(requestJson, req);
     }
 
-    // var httpClient = new HttpClient();
-    // var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://catfact.ninja/fact");
-    // requestMessage.Headers.Add("jacob", new []{"test"});
-    // var response = await httpClient.SendAsync(requestMessage);
-    // string body = response.Content.ReadAsStringAsync().Result;
+    // create upstream request
+    var upstreamRequest = new UpStreamRequest { Val1 = req.Num1, Val2 = req.Num2 };
+    var downStreamReqBody = new StringContent(
+       JsonConvert.SerializeObject(upstreamRequest),
+       Encoding.UTF8,
+       Application.Json);
 
-    string name = req.Name;
-    string responseMessage = string.IsNullOrEmpty(name)
-        ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-        : $"Hello, {name}. This HTTP triggered function executed successfully.";
+    var httpClient = new HttpClient();
 
-    var result = new Message { Name = name, ReturnMessage = responseMessage };
-    string body = JsonConvert.SerializeObject(result);
+    // send request to upstream server, async call as await switches context to a worker thread
+    var upstreamResponse = await MakeAsyncPostRequest(httpClient, "http://localhost:5000/add", downStreamReqBody);
+
+    // populate result into response of server
+    req.Num1 = upstreamResponse.Result;
+
+    // hardcoded delay
+    string minDelay = Environment.GetEnvironmentVariable("MIN_DELAY_TO_RETURN", EnvironmentVariableTarget.Machine);
+    int minDelayValue;
+    if (!int.TryParse(minDelay, out minDelayValue))
+    {
+        minDelayValue = 20;
+    }
+
+    while (DateTimeOffset.Now.ToUnixTimeMilliseconds() - start < minDelayValue)
+    {
+        // send requests to upstream server, sync call as Task.Result will block the current thread
+        var httpResponseMessage = httpClient.PostAsync("http://localhost:5000/sumOfSquares", downStreamReqBody).Result;
+        upstreamResponse = new UpStreamResponse();
+        var upstreamRequestJson = httpResponseMessage.Content.ReadAsStringAsync().Result;
+        upstreamResponse = JsonConvert.DeserializeAnonymousType(upstreamRequestJson, upstreamResponse);
+
+        // populate result into response of server
+        req.Num2 = upstreamResponse.Result;
+    } // introduce delay to match real world cases
+
+    // send response back to client
+    string responseJson = JsonConvert.SerializeObject(req);
     context.Response.ContentType = "application/json";
-    context.Response.ContentLength = body.Length;
-    await context.Response.Body.WriteAsync(Encoding.ASCII.GetBytes(body));
+    context.Response.ContentLength = responseJson.Length;
+    await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(responseJson));
 });
 
 
 app.Run();
 
+async Task<UpStreamResponse> MakeAsyncPostRequest(HttpClient httpClient, string uri, StringContent reqBody)
+{
+    var httpResponseMessage = await httpClient.PostAsync("http://localhost:5000/add", reqBody);
+    var upstreamResponse = new UpStreamResponse();
+    var upstreamRequestJson = await httpResponseMessage.Content.ReadAsStringAsync();
+    upstreamResponse = JsonConvert.DeserializeAnonymousType(upstreamRequestJson, upstreamResponse);
+    return upstreamResponse;
+}
 
+
+public class UpStreamResponse
+{
+    int result;
+    string message;
+
+    public int Result
+    {
+        get { return this.result; }
+        set { this.result = value; }
+
+    }
+    public string ReturnMessage
+    {
+        get { return this.message; }
+        set { this.message = value; }
+
+    }
+
+}
+
+public class UpStreamRequest
+{
+    public int Val1 { get; set; }
+    public int Val2 { get; set; }
+
+}
 public class Message
 {
     string _name;
     string _message;
+    string _address;
 
     public string Name
     {
@@ -51,16 +115,41 @@ public class Message
         set { this._name = value; }
 
     }
-        public string ReturnMessage
+    public string ReturnMessage
     {
         get { return this._message; }
         set { this._message = value; }
 
+    }
+    public string Address
+    {
+        get { return this._address; }
     }
 
 }
 
 public class Request
 {
-    public string Name {get;set;}
+    public List<DataItem> Data { get; set; }
+    public string Authorization { get; set; }
+    public string Additional { get; set; }
+    public int Num1 { get; set; }
+    public int Num2 { get; set; }
+
+    public class DataItem
+    {
+        public string Name { get; set; }
+        public int Telephone { get; set; }
+        public string Curr_City { get; set; }
+        public string Weather { get; set; }
+        public List<NestedItem> Nested { get; set; }
+    }
+
+    public class NestedItem
+    {
+        public string Name { get; set; }
+        public int Telephone { get; set; }
+        public string Curr_City { get; set; }
+        public string Weather { get; set; }
+    }
 }
