052ca9a5486bc8ac77bae57f89452147c71d9e75
test <test@test-ka-MacBook-Pro.local>
2024-06-26 00:16:15 +0530

    adding grpc client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcServer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        List<string> users = ["Liam", "Olivia", "Noah", "Emma", "Oliver", "Charlotte", "James",	"Amelia", "Elijah", "Sophia"];
+
+        GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:8080");
+        var client = new CatWorld.CatWorldClient(channel);
+
+        var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+        using var call = client.CatFactStream(cancellationToken: cancellationToken.Token);
+
+        Task task = Task.WhenAll(new []
+        {
+            // write to stream
+            Task.Run(async()=> {
+                foreach(var user in users)
+                {
+                    await call.RequestStream.WriteAsync(new CatFactRequest{
+                        Name = user
+                    });
+                }
+
+                await call.RequestStream.CompleteAsync();
+            }),
+
+            // read from stream
+            Task.Run(async() => {
+                while(!cancellationToken.IsCancellationRequested && await call.ResponseStream.MoveNext(cancellationToken.Token))
+                {
+                    Console.WriteLine(call.ResponseStream.Current);
+                }
+            })
+
+        });
+
+        task.Wait();
+    }
+}
--- GrpcServer/Services/GreeterService.cs
+++ GrpcServer/Services/GreeterService.cs
@@ -12,6 +12,20 @@ public class CatFactService(ILogger<CatFactService> logger, HttpClient client) :
     public override async Task<CatFactResponse> CatFact(CatFactRequest request, ServerCallContext context)
     {
 
+        return await UpstreamService(request);
+    }
+
+    public override async Task CatFactStream(IAsyncStreamReader<CatFactRequest> requestStream, IServerStreamWriter<CatFactResponse> responseStream, ServerCallContext context)
+    {
+        while(await requestStream.MoveNext() && !context.CancellationToken.IsCancellationRequested)
+        {
+            var request = requestStream.Current;
+            await responseStream.WriteAsync(await UpstreamService(request));
+        }
+    }
+
+    private async Task<CatFactResponse> UpstreamService(CatFactRequest request)
+    {
         var response = await httpClient.GetAsync("https://catfact.ninja/fact");
         var factJson = await response.Content.ReadAsStringAsync(); 
         FactResponse fact = new();
