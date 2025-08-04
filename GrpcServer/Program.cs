7c5eb437007d395feceb96d7d622c84b20166de4
Varkeychan Jacob <varkey98@gmail.com>
2025-08-04 15:05:27 +0530

    fix dockerfile

--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -26,7 +26,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<CatFactService>();
-app.MapGrpcService<EdgeDecisionServiceTest>();
 app.MapGrpcService<HealthService>(); // Health check endpoint
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
--- GrpcServer/Services/EdgeDecisionService.cs
+++ GrpcServer/Services/EdgeDecisionService.cs
@@ -1,34 +1,34 @@
-namespace GrpcServer.Services;
+// namespace GrpcServer.Services;
 
-using Grpc.Core;
-using Ai.Traceable.Edge.Decision.Service.Api.V1;
+// using Grpc.Core;
+// using Ai.Traceable.Edge.Decision.Service.Api.V1;
 
-public class EdgeDecisionServiceTest : EdgeDecisionService.EdgeDecisionServiceBase
-{
-    private readonly ILogger<EdgeDecisionServiceTest> logger;
+// public class EdgeDecisionServiceTest : EdgeDecisionService.EdgeDecisionServiceBase
+// {
+//     private readonly ILogger<EdgeDecisionServiceTest> logger;
 
-    public EdgeDecisionServiceTest(ILogger<EdgeDecisionServiceTest> logger) {
-        this.logger = logger;
-    }
+//     public EdgeDecisionServiceTest(ILogger<EdgeDecisionServiceTest> logger) {
+//         this.logger = logger;
+//     }
 
-    public override async Task<AssessRiskResponse> AssessRisk(AssessRiskRequest request, ServerCallContext context)
-    {
-        DateTime deadline = context.Deadline;
+//     public override async Task<AssessRiskResponse> AssessRisk(AssessRiskRequest request, ServerCallContext context)
+//     {
+//         DateTime deadline = context.Deadline;
     
-        // Check if the deadline is set
-        if (deadline != DateTime.MaxValue)
-        {
-            Console.WriteLine($"{DateTime.Now.Millisecond}: {deadline.Millisecond}");
-        }
-        foreach(var header in context.RequestHeaders)
-        {
-            Console.WriteLine($"{header.Key} : {header.Value}");
-        }
+//         // Check if the deadline is set
+//         if (deadline != DateTime.MaxValue)
+//         {
+//             Console.WriteLine($"{DateTime.Now.Millisecond}: {deadline.Millisecond}");
+//         }
+//         foreach(var header in context.RequestHeaders)
+//         {
+//             Console.WriteLine($"{header.Key} : {header.Value}");
+//         }
 
-        foreach (var attr in request.GenericRequest.InputPayload) 
-        {
-            Console.WriteLine($"{attr.Key} : {attr.Value}");
-        }
-        return new AssessRiskResponse();
-    }
-}
+//         foreach (var attr in request.GenericRequest.InputPayload) 
+//         {
+//             Console.WriteLine($"{attr.Key} : {attr.Value}");
+//         }
+//         return new AssessRiskResponse();
+//     }
+// }
